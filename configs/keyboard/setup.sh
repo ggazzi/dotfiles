6c604a63b8724c6d08439560bd57fff76769282a
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2016-08-08 12:07:26 -0300

    Add keyboard configuration

---
+++ configs/keyboard/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+make_link()
+{
+  local name="$1"
+  local target="$(realpath "$CONFIG_ROOT/$1")"
+  local link_path="$2"
+
+  if [ -f "$link_path" ] || [ -L "$link_path" ]
+  then
+      echo "backing up previous $name"
+      mv "$link_path" "$link_path.prev"
+  fi
+
+  echo "linking $name"
+  ln -s "$target" "$link_path"
+
+  echo
+}
+
+set -x
+
+CONFIG_ROOT="$(realpath "$1")"
+
+make_link XCompose "$HOME/.XCompose"
+
+mkdir -p "$HOME/.bashrc.d"
+make_link bashrc_ibus "$HOME/.bashrc.d/0_ibus"

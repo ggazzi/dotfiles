dc2fa17d3121f25fae3f6b9ac8476dc9150ef08e
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2017-05-16 13:31:54 -0300

    Add qt-gtk integration packages to setup

--- setup-scripts/cinnamon.sh
+++ setup-scripts/cinnamon.sh
@@ -9,12 +9,12 @@ sudo pacman -S cinnamon
 echo
 echo "Installing themes"
 sudo pacman -S faenza-icon-theme faience-icon-theme otf-fira-mono otf-fira-sans
-aurget -S gtk-theme-arc ttf-roboto ttf-roboto-mono otf-fira-code otf-hasklig --noedit
+aurget -S gtk-theme-arc ttf-roboto ttf-roboto-mono otf-fira-code otf-hasklig qt5-styleplugins --noedit
 
 
 echo
 echo "Installing utilities"
-sudo pacman -S file-roller gnome-terminal gedit firefox system-config-printer
+sudo pacman -S file-roller gnome-terminal gedit firefox system-config-printer konsole
 
 
 echo

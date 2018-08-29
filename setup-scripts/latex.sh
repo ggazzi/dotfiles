f9b7d7aa4941e6eea3547f6813ba576d56bc0ca3
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2018-08-29 09:41:55 -0300

    Configure flycheck for Emacs

--- setup-scripts/latex.sh
+++ setup-scripts/latex.sh
@@ -4,3 +4,6 @@
 echo
 echo "Installing texlive"
 sudo pacman -S texlive-{bibtexextra,bin,core,fontsextra,formatsextra,langextra,latexextra,pictures,science} biber
+
+echo "Installing auctex"
+sudo pacman -S auctex

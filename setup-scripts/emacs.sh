0424e6ac54e1c8801da6ee5a1ecd50ef2d795b1f
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2018-08-29 15:32:03 -0300

    Update emacs install script

--- setup-scripts/emacs.sh
+++ setup-scripts/emacs.sh
@@ -3,6 +3,9 @@
 # Install emacs
 sudo pacman -S emacs
 
+# Install spell checking software
+sudo pacman -S aspell aspell-de aspell-en aspell-pt
+
 # Install isync/mbsync for fetching email, notmuch for reading email
 sudo pacman -S isync notmuch
 pip install --user notmuch

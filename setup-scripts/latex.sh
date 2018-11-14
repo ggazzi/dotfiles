7739128038151e257ea3ecc66b72fd9c8394f5af
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2018-11-14 13:44:55 +0100

    Update installation script for LaTeX

--- setup-scripts/latex.sh
+++ setup-scripts/latex.sh
@@ -3,7 +3,7 @@
 
 echo
 echo "Installing texlive"
-sudo pacman -S texlive-{bibtexextra,bin,core,fontsextra,formatsextra,langextra,latexextra,pictures,science} biber
+sudo pacman -S texlive-{bibtexextra,bin,core,fontsextra,formatsextra,langextra,latexextra,pictures,science,publishers} biber
 
 echo "Installing auctex"
 sudo pacman -S auctex

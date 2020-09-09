048061ce7a6de5f712dd579c7576ef03d90ae1ff
Guilherme Grochau Azzi <g.grochauazzi@tu-berlin.de>
2020-09-09 12:37:35 +0200

    [setup-scripts] Clean up

--- setup-scripts/gnome.sh
+++ setup-scripts/gnome.sh
@@ -3,7 +3,7 @@
 
 echo
 echo "Installing Gnome"
-sudo pacman -S adwaita-icon-theme baobab eog evince gnome-backgrounds gnome-calculator gnome-control-center gnome-dictionary gnome-disk-utility gnome-font-viewer gnome-keyring gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-themes-standard gnome-user-docs gnome-user-share grilo-plugins gtk3-print-backends gucharmap gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb mutter nautilus networkmanager sushi tracker xdg-user-dirs-gtk yelp gnome-tweak-tool gnome-calendar dconf-editor gnome-contacts gnome-documents gnome-weather evolution vlc ffmpegthumbnailer xorg-xrandr
+sudo pacman -S gnome dconf-editor gnome-tweaks
 
 echo
 echo "Installing themes"
@@ -24,8 +24,15 @@ echo
 echo "Installing gnome shell extensions"
 aurget -S --noedit chrome-gnome-shell-git
 
-
 echo
 echo "Installing utilities to backup/restore config"
 sudo pacman -S python-pip
 pip install --user pyyaml
+
+echo
+echo "Installing automatic wallpaper switcher"
+sudo pacman -S variety
+
+echo
+echo "Installing file sync"
+aurget -S --noedit dropbox dropbox-cli nautilus-dropbox

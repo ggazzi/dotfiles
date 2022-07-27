a787e5329bb9c6baaf797d3470cc0d7af9d25e16
Guilherme Grochau Azzi <gui.g.azzi@gmail.com>
2022-07-27 11:43:54 +0200

    Add MIME associations for gnome

--- modules/system/desktop/default.nix
+++ modules/system/desktop/default.nix
@@ -41,7 +41,7 @@ in {
     # Enable the GNOME Desktop Environment.
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
-    
+
     # Configure keymap in X11
     services.xserver.layout = "us";
     services.xserver.xkbVariant = "intl";
@@ -62,5 +62,20 @@ in {
 
     # Enable touchpad support (enabled default in most desktopManager).
     # services.xserver.libinput.enable = true;
+
+    # Set some default MIME associations
+    xdg.mime = {
+      enable = true;
+      addedAssociations = {
+        "application/pdf" = "evince.desktop";
+
+        "application/x-yaml" = "org.gnome.TextEditor.desktop";
+        "text/plain" = "org.gnome.TextEditor.desktop";
+      };
+      defaultApplications = {
+        "application/pdf" = "evince.desktop";
+        "inode/directory" = "org.gnome.Nautilus.desktop";
+      };
+    };
   };
 }

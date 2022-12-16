f781ef289182027e75be5cd4a46fc7717200b219
Guilherme Grochau Azzi <gui.g.azzi@gmail.com>
2022-12-16 17:08:11 +0100

    Add doom emacs config to the repo

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -1,7 +1,7 @@
 { pkgs, config, lib, ... }:
 
 {
-  config = {
+  config = let inherit (config) xdg; in {
     # I am not managing doom emacs with nix.
 
     # I only install some dependencies for doom
@@ -17,8 +17,8 @@
 
     home.sessionVariables = {
       # Set the directories where the doom configuration should go
-      DOOMDIR = "${config.xdg.configHome}/doom-emacs";
-      DOOMLOCALDIR = "${config.xdg.dataHome}/doom-emacs";
+      DOOMDIR = "${xdg.configHome}/doom-emacs";
+      DOOMLOCALDIR = "${xdg.dataHome}/doom-emacs";
 
       # Use as default command-line editor
       EDITOR = "emacs -nw";
@@ -31,6 +31,12 @@
       ".dir-locals.el"
       "*.elc"
     ];
+
+    xdg.configFile = {
+      "doom-emacs/config.el".source = ./config.el;
+      "doom-emacs/init.el".source = ./init.el;
+      "doom-emacs/packages.el".source = ./packages.el;
+    };
 
   };
 }

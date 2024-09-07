d4829207bf27c0829a52d4a3de1412948a31ede0
Guilherme Grochau Azzi <gui.g.azzi@gmail.com>
2024-09-07 12:51:01 +0200

    add nix-darwin configuration

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -37,6 +41,13 @@
 
     in
     {
+      darwinConfigurations."Guilhermes-MacBook-Air" =
+        inputs.nix-darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          modules = [
+            ./Guilhermes-MacBook-Air/configuration.nix 
+          ];
+        };
 
       homeConfigurations.gazzi =
         home-manager.lib.homeManagerConfiguration {

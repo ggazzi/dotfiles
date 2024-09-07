2484dccd7df40e8f44af613817e4e014e858aba1
Guilherme Grochau Azzi <gui.g.azzi@gmail.com>
2024-09-07 12:51:01 +0200

    darwin: add nix-darwin configuration

---
+++ Guilhermes-MacBook-Air/configuration.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    curl
+    gitAndTools.gitFull
+  ];
+
+  users.users.gazzi.home = "/Users/gazzi";
+  
+  # Auto-upgrade nix package and the daemon service
+  services.nix-daemon.enable = true;
+
+  nix.settings.experimental-features = "nix-command flakes";
+
+  programs.zsh.enable = true;
+  
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  nix.configureBuildUsers = true;
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -43,6 +47,13 @@
 
     in
     {
+      darwinConfigurations."Guilhermes-MacBook-Air" =
+        inputs.nix-darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          modules = [
+            ./Guilhermes-MacBook-Air/configuration.nix 
+          ];
+        };
 
       homeConfigurations.gazzi =
         home-manager.lib.homeManagerConfiguration {

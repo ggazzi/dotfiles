b5f404731ddc44739263ecdf7c05e24beeb0a06d
Guilherme Grochau Azzi <gazzi@babbel.com>
2024-10-10 23:59:27 +0200

    nixvim: improve config

--- nixvim/config/coding/default.nix
+++ nixvim/config/coding/default.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./comments.nix
@@ -11,11 +13,37 @@
   ];
 
   config = {
-    plugins.which-key.settings.spec = [
-      { __unkeyed-1 = "<leader>c"; group = "Code"; }
-    ];
+    plugins = {
+      which-key.settings.spec = [
+        { __unkeyed-1 = "<leader>c"; group = "Code"; }
+      ];
 
-    # Detect tabstop and shiftwidth automatically
-    plugins.sleuth.enable = true;
+      # Detect tabstop and shiftwidth automatically
+      sleuth.enable = true;
+
+      nvim-surround.enable = true;
+
+      trim = {
+        enable = true;
+        settings = {
+          highlight = true;
+          trim_on_write = true;
+          trim_trailing = true;
+          trim_last_line = true;
+        };
+      };
+    };
+
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "targets-vim";
+        src = pkgs.fetchFromGitHub {
+          owner = "wellle";
+          repo = "targets.vim";
+          rev = "master";
+          sha256 = "sha256-ThfL4J/r8Mr9WemSUwIea8gsolSX9gabJ6T0XYgAaE4=";
+        };
+      })
+    ];
   };
 }

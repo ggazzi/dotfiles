4cd491511b41b184364a7d037697eea67e2e9ccc
Guilherme Grochau Azzi <gazzi@babbel.com>
2024-11-05 15:50:16 +0100

    nixvim: set up auto-save and auto-load

--- nixvim/config/default.nix
+++ nixvim/config/default.nix
@@ -7,4 +7,27 @@
     ./keys.nix
     ./navigation
   ];
+
+  opts = {
+    # Make sure files are auto-saved and loaded all the time
+    autowriteall = true;
+    autoread = true;
+    hidden = true;
+    updatetime = 200;
+
+    # We don't need swapfiles/recovery if we're saving all the time
+    swapfile = false;
+  };
+
+  # Ensure files are saved as soon as we're not working on them
+  autoCmd = [
+    {
+      event = "BufHidden";
+      command = "silent! write";
+    }
+    {
+      event = "FocusLost";
+      command = "silent! wall";
+    }
+  ];
 }

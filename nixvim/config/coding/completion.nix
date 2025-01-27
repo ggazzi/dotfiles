8ccc18a7ea69dfd83d8a950b44ea46237a978780
Guilherme Grochau Azzi <gui.g.azzi@gmail.com>
2025-01-27 16:25:08 +0100

    nixvim: fix completion keybindings

--- nixvim/config/coding/completion.nix
+++ nixvim/config/coding/completion.nix
@@ -1,28 +1,20 @@
 {
-  plugins = {
-    cmp = {
-      enable = true;
-      settings = {
-        completion.autocomplete = false;
+  plugins.cmp = {
+    enable = true;
+    autoEnableSources = true;
+
+    settings = {
+      # Only trigger completion manually
+      completion.autocomplete = false;
+
+      mapping = {
+        "<C-i>" = "cmp.mapping(cmp.mapping.complete(), { 'i' })";
+        "<C-k>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-j>" = "cmp.mapping.scroll_docs(4)";
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "S-Tab" = "cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })";
+        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' })";
       };
-
-      autoEnableSources = true;
-      settings.sources = [
-        { name = "nvim_lsp"; }
-      ];
     };
-
-    cmp-nvim-lsp.enable = true;
-
-    lspkind.enable = true;
   };
-
-  keymaps = [
-    {
-      mode = "i";
-      key = "<C-Space>";
-      action = "cmp#complete()";
-      options = { noremap = true; silent = true; };
-    }
-  ];
 }

bb7caa3b2c28a3fe19406ce4fcf497e69e70005e
Guilherme Grochau Azzi <gui.g.azzi@gmail.com>
2025-01-27 17:14:13 +0100

    nixvim: clean up copilot config

--- nixvim/config/coding/copilot.nix
+++ nixvim/config/coding/copilot.nix
@@ -1,8 +1,4 @@
-{ mylib, ... }:
-
 {
-  # plugins.copilot-vim.enable = true;
-
   plugins.copilot-lua = {
     enable = true;
 
@@ -19,15 +15,4 @@
       };
     };
   };
-
-  plugins.which-key.settings.spec = [
-    { __unkeyed-1 = "<leader>cC"; group = "Copilot"; }
-  ];
-
-  keymaps = mylib.keymapGroup "<leader>cC" [
-    { key = "e"; action = ":Copilot enable<CR>"; options.desc = "Enable Copilot"; }
-    { key = "d"; action = ":Copilot disable<CR>"; options.desc = "Disable Copilot"; }
-    { key = "s"; action = ":Copilot status<CR>"; options.desc = "Show Copilot status"; }
-    { key = "C"; action = ":Copilot panel<CR>"; options.desc = "Show panel with up to 10 completions"; }
-  ];
 }

b20276b829a1740d2f8aa901eac33e76ce2acf0c
Guilherme Grochau Azzi <gui.g.azzi@gmail.com>
2026-09-03 14:14:42 +0200

    feat(chezmoi): migrate dev-utils and add dev dispatcher (U3)

---
+++ private_dot_local/libexec/dev-utils/lib/interactive.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Ask a user a binary question.
+#
+# Arguments:
+#  - $1: Question to be asked.
+#  - $2: (optional) Default choice, must be "Y" or "N".
+#
+# Exit codes:
+#  - 0: user answered with yes
+#  - 1: user answered with no
+ask_yes_or_no() {
+    local default=$2
+    local prompt=$1
+
+    case $default in
+    [Yy]*) prompt="$prompt [Y/n]" ;;
+    [Nn]*) prompt="$prompt [y/N]" ;;
+    *) prompt="$prompt [y/n]" ;;
+    esac
+
+    local choice
+    while true; do
+        read -rp "$prompt " choice
+        case $choice in
+        [Yy]*) return 0 ;;
+        [Nn]*) return 1 ;;
+        "")
+            case $default in
+            [Yy]*) return 0 ;;
+            [Nn]*) return 1 ;;
+            *) echo "Please answer yes or no." ;;
+            esac
+            ;;
+        *) echo "Please answer yes or no." ;;
+        esac
+    done
+}
